2c41b6132aa8493eb97025e5b5395224c261d37e
datamannen1013 <data0@rommesmo.no>
2025-04-15 21:00:21 +0200

    Huffman files, no implementation

---
+++ FileCompression/CompressionMethods/AssistAlgorithms/Huffman/Huffman.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 15.04.2025.
+//
+
+#include "Huffman.h"
---
+++ FileCompression/CompressionMethods/AssistAlgorithms/Huffman/Huffman.h
@@ -0,0 +1,16 @@
+//
+// Created by admin on 15.04.2025.
+//
+
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+
+
+class Huffman {
+
+};
+
+
+
+#endif //HUFFMAN_H
